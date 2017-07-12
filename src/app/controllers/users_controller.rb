8191e33244be39f078a8383f484e1eea480bd6ea
assefamaru <me@alexandermaru.com>
2017-07-12 00:41:47 -0400

    Hard reset, prep for golang migration

--- src/app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-
-  private
-
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:name, :email, :api_key)
-    end
-end
