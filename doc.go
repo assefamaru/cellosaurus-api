c644fb4ccea3ee9cb520120a251283581ea65bb7
assefamaru <me@alexandermaru.com>
2017-09-14 18:13:31 -0400

    Hard reset back to the future.

--- doc.go
+++
@@ -1,7 +0,0 @@
-/*
-Package cellosaurus is an API, providing programmatic access to Cellosaurus data.
-
-Cellosaurus data can be found at: http://web.expasy.org/cellosaurus/.
-For api documentation, visit: https://github.com/assefamaru/cellosaurus-api.
-*/
-package cellosaurus
