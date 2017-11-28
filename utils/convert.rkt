15460d01b476985073e0c04dad6b61b01c134015
assefamaru <me@alexandermaru.com>
2017-11-28 02:00:48 -0500

    Update convert module, build script for creating database

--- utils/convert.rkt
+++ utils/convert.rkt
@@ -7,7 +7,7 @@
 #lang racket
 
 ;; Headers defining the list of column names for the csv files.
-(define header-cells "\"\",\"identifier\",\"accession\"")
+(define header-cells "\"accession\",\"identifier\"")
 (define header-attrs "\"\",\"accession\",\"attribute\",\"content\"")
 (define header-refs "\"\",\"ref_identifier\",\"attribute\",\"content\"")
 
@@ -17,17 +17,16 @@
 ;; For example, lines such as:
 ;;                            "ID   #15310-LN"
 ;;                            "AC   CVCL_E548"
-;;              gets stored in csv as: n,"#15310-LN","CVCL_E548"
-;;              whereby n is some row number (integer) > 0
-(define (write-cells-to-csv in [row 1])
+;;              gets stored in csv as: "CVCL_E548","#15310-LN"
+(define (write-cells-to-csv in)
   (define line (read-line in))
   (unless (eof-object? line)
     (define id (cadr (string-split line "   ")))
     (define ac (cadr (string-split (read-line in) "   ")))
-    (printf "~a,\"~a\",\"~a\"\n" row id ac)
+    (printf "\"~a\",\"~a\"\n" ac id)
     (let loop ()
       (if (equal? (read-line in) "//")
-          (write-cells-to-csv in (add1 row))
+          (write-cells-to-csv in)
           (loop)))))
 
 ;; write-attrs-to-csv iterates over each line of cellosaurus.txt,
@@ -49,7 +48,7 @@
          (printf "~a,\"~a\",\"~a\",\"~a\"\n" row ac (car lst) (cadr lst))
          (loop (add1 row))]))))
 
-;; write-attrs-to-csv iterates over each line of cellosaurus.txt,
+;; write-attrs-to-csv iterates over each line of cellosaurus_refs.txt,
 ;; parsing line and writing content to csv (via printf).
 (define (write-refs-to-csv in [row 1])
   (define line (read-line in))
@@ -65,29 +64,28 @@
          (printf "~a,\"~a\",\"~a\",\"~a\"\n" row rx (car lst) (cadr lst))
          (loop (add1 row))]))))
 
-;; Convert cellosaurus.txt to csv format.
+;; Convert Cellosaurus' data from txt to csv format.
 (define (convert txt csv table)
   (define in (open-input-file txt))
   (with-output-to-file csv
     (lambda ()
       (cond
-        [(equal? table "cell_lines")
+        [(equal? table "cells")
          (printf "~a\n" header-cells)
          (write-cells-to-csv in)]
         [(equal? table "attributes")
          (printf "~a\n" header-attrs)
          (write-attrs-to-csv in)]
-        [(equal? table "references")
+        [(equal? table "refs")
          (printf "~a\n" header-refs)
          (write-refs-to-csv in)])))
   (close-input-port in))
 
-;; --- Uncomment below to convert .txt to .csv
 ;; --- "cells.csv" output will contain unique cell lines,
 ;;      each with their identifier and accession attributes only.
 ;; --- "attributes.csv" ouput will contain each attribute type and attribute content
 ;;      for a cell line (referenced using accession).
-;; --- "references.csv" output will contain reference data for each reference identifier
-;; (convert "cellosaurus.txt" "cells.csv" "cell_lines")
-;; (convert "cellosaurus.txt" "attributes.csv" "attributes")
-;; (convert "cellosaurus_refs.txt" "references.csv" "references")
+;; --- "refs.csv" output will contain reference data for each reference identifier
+(convert "../data/txt/cellosaurus.txt" "../data/csv/cells.csv" "cells")
+(convert "../data/txt/cellosaurus.txt" "../data/csv/attributes.csv" "attributes")
+(convert "../data/txt/cellosaurus_refs.txt" "../data/csv/refs.csv" "refs")
