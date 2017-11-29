2e3624629851c47ccf5e85fdde23ca3875052e75
assefamaru <me@alexandermaru.com>
2017-11-29 01:39:41 -0500

    Put basic documentation for api package godoc

---
+++ src/doc.go
@@ -0,0 +1,12 @@
+/*
+Package cellosaurus is an API for the Cellosaurus database.
+
+The Cellosaurus is publicly accessible at either of:
+ - https://web.expasy.org/cellosaurus/
+ - https://github.com/calipho-sib/cellosaurus
+
+This API, and all its usage documentations, are available at either of:
+ - https://cellosaur.us
+ - https://github.com/assefamaru/cellosaurus-api
+*/
+package cellosaurus
